cd381e5e52713bdc543aa2744c2c37eea2af37ac
william-zhang-ml <william.zhang.ml@gmail.com>
2022-06-16 18:51:08 -0400

    added feature pyramid encoder

--- wcztools/featpyra.py
+++ wcztools/featpyra.py
@@ -7,12 +7,13 @@ References:
 [1] Feature Pyramid Networks for Object Detection
     https://arxiv.org/pdf/1612.03144.pdf
 """
+from typing import List
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 
-class FeaturePyramidLink(nn.Module):
+class FeaturePyramidBlock(nn.Module):
     """
     Fusion block for top-down lower res. features and higher res. features.
 
@@ -61,3 +62,46 @@ class FeaturePyramidLink(nn.Module):
         proj = self.projection(highres)
         fused = upsamped + proj
         return self.filter(fused)
+
+
+class FeaturePyramidEncoder(nn.Module):
+    """
+    Top-down fusion network for encoding multi-scale features.
+    """
+    def __init__(self,
+                 in_channels: List[int],
+                 out_channels: int) -> None:
+        """
+        Constructor. Set up initial projection layer and fusion blocks.
+
+        Args:
+            in_channels:  number of input channels (top-to-bottom)
+            out_channels: desired number of top-down path channels
+        """
+        super().__init__()
+        self.projection = nn.Conv2d(
+            in_channels=in_channels[0],
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0)
+        self.blocks = nn.ModuleList([
+            FeaturePyramidBlock(in_chan, out_channels)
+            for in_chan in in_channels[1:]
+        ])
+
+    def forward(self, featmaps: List[torch.Tensor]) -> List[torch.Tensor]:
+        """
+        Iteratively fuse features for multi-scale feature maps.
+
+        Args:
+            featmaps: feature maps to fuse (lowres-to-highres)
+
+        Returns: multi-scale feature maps at different resolutions
+        """
+        fused = [self.projection(featmaps[0])]  # start top-down path
+        topdown = fused[0]
+        for lateral, block in zip(featmaps[1:], self.blocks):
+            topdown = block(topdown, lateral)
+            fused.append(topdown)
+        return fused
